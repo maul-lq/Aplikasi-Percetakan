d26a96ffb295536e3325d5474c00e5f0af9315c8
Rizlrad Fz <rizlradfz@gmail.com>
2024-12-10 11:32:04 +0700

    Start Poin

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+#include <ctype.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /*Isi kode disini*/
+
+    cout << endl;    
+    return 0;
+}
