3a1424c96d1c4da88716c1d05a5a6d080e450dea
Rizlrad Fz <rizlradfz@gmail.com>
2024-12-11 22:08:22 +0700

    Update v10091511122024 Fitur Login, Register, (Tampilan Menu Utama,) dan Logou selesai. tinggal penyesuaian dari fitur lain.
    	modified:   .vscode/settings.json
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@ void CetakJudulAplikasi()
     cout << endl;
 }
 
-
-
+bool keluar = false;
 string pilihan_pengguna;
+bool isLogin = false;
 #pragma endregion
 
 #pragma region Properti Akun
@@ -51,7 +51,8 @@ string pilihan_pengguna;
 enum Role
 {
     Admin = 0x1A1,
-    User = 0x1A2
+    User = 0x1A2,
+    Null = 0x100
 };
 struct Akun
 {
@@ -64,15 +65,118 @@ struct Akun
 // agar bisa membedakan mana yang akun Admin dna User.
 // Buat variabel array berupa vector yang fungsinya untuk menyimpan akun pengguna aplikasi.
 vector<pair<int, Akun>> Database_Akun = {
-    {1, {"Admin", "081585893549", "admin", "admin123", Admin}},
-};
+    {1, {"Admin", "084277744102", "admin", "admin123", Admin}}};
 
 Akun session; // Digunakan untuk menyimpan data akun saat sudah login
+
 #pragma endregion
 
-int main()
+#pragma region Login Regiser Logout
+void LogoutLogic()
 {
-    while (true)
+    isLogin = false;
+    session.nama="";
+    session.nomor_hp="";
+    session.username="";
+    session.password="";
+    session.role = Null;
+}
+
+bool LoginLogic(string username, string password)
+{
+    // Cek username dan password di database
+    for (pair<int, Akun> akun : Database_Akun)
+    {
+        if (akun.second.username == username && akun.second.password == password)
+        {
+            session = akun.second;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool RegisterLogic(string nama, string nomor_hp, string username, string password)
+{
+    bool isUsernameExist = false;
+    for (pair<int, Akun> akun : Database_Akun)
+    {
+        if (akun.second.username == username)
+        {
+            isUsernameExist = true;
+            break;
+        }
+    }
+
+    if (isUsernameExist)
+    {
+        return false;
+    }
+    else
+    {
+        // Buat id baru
+        int id = Database_Akun.size() + 1;
+        // Simpan akun ke database
+        Database_Akun.push_back({id, {nama, nomor_hp, username, password, User}});
+        return true;
+    }
+}
+
+void Tampilan_Login()
+{
+    string username, password;
+
+    // Tampilan Login
+    CetakJudulAplikasi();
+    cout << "Silahkan masukan username dan password untuk login" << endl;
+    cout << "Username: ";
+    getline(cin, username);
+    cout << "Password: ";
+    getline(cin, password);
+
+    if (LoginLogic(username, password))
+    {
+        isLogin = true;
+        // cout << "Login berhasil!" << endl;
+        // system("pause");
+        // system("cls");
+    }
+    else
+    {
+        cout << "Username/Password salah! Silahkan coba lagi." << endl;
+        system("pause");
+        system("cls");
+    }
+}
+
+void Tampilan_Register()
+{
+    string nama, nomor_hp, username, password;
+
+    // Tampilan Regiser
+    CetakJudulAplikasi();
+    cout << "Halo, untuk membuat akun baru.\n"
+         << "Silahkan masukan Nama, Nomor Hp, Username, dan Password!" << endl;
+    cout << "Nama Lengkap\t: ";
+    getline(cin, nama);
+    cout << "Nomor Hp\t: ";
+    getline(cin, nomor_hp);
+    cout << "Username\t: ";
+    getline(cin, username);
+    cout << "Password\t: ";
+    getline(cin, password);
+
+    if (!RegisterLogic(nama, nomor_hp, username, password))
+    {
+        cout << "Username telah tersedia!" << endl;
+        system("pause");
+        system("cls");
+    }
+}
+
+void Tampilan_LoginRegister()
+{
+    while (!isLogin)
     {
         CetakJudulAplikasi();
         cout << "1. Login" << endl;
@@ -82,23 +186,91 @@ int main()
         getline(cin, pilihan_pengguna);
         if (pilihan_pengguna == "1")
         {
-            // Tampilan Login
+            Tampilan_Login();
         }
         else if (pilihan_pengguna == "2")
         {
-            // Tampilan  Register
+            Tampilan_Register();
         }
         else if (pilihan_pengguna == "3")
         {
+            keluar = true;
+
             break;
-        }else
+        }
+        else
         {
             cout << "Pilihan yang anda pilih tidak valid! Coba ulangi." << endl;
             system("pause");
             system("cls");
         }
     }
+}
+#pragma endregion
+
+#pragma region Menu Utama
+void TampilanMenuUtamaAdmin()
+{
+    CetakJudulAplikasi();
+    cout << "Selamat datang, " << session.nama << "!" << endl;
+    cout << "1. Data Pelanggan" << endl;
+    cout << "2. Katalog & Harga" << endl;
+    cout << "3. Laporan Keuangan" << endl;
+    cout << "4. Konfirmasi Pemesanan" << endl;
+    cout << "5. Logout" << endl;
+    cout << "Masukkan pilihan anda: \n>> ";
+}
+
+void TampilanMenuUtamaUser()
+{
+    CetakJudulAplikasi();
+    cout << "Selamat datang, " << session.nama << "!" << endl;
+    cout << "1. Katalog & Harga" << endl;
+    cout << "2. Form Pemesanan" << endl;
+    cout << "3. Informasi Pemesanan" << endl;
+    cout << "4. Status Pemesanan" << endl;
+    cout << "5. Booking" << endl;
+    cout << "6. Logout" << endl;
+    cout << "Masukkan pilihan anda: \n>> ";
+}
+#pragma endregion
+
+#pragma region Main Program
+int main()
+{
+    while (!keluar)
+    {
+        if (!isLogin)
+        {
+            Tampilan_LoginRegister();
+        }
+        else
+        {
+            if (session.role == Admin)
+            {
+                TampilanMenuUtamaAdmin();
+                getline(cin, pilihan_pengguna);
+                if (pilihan_pengguna == "5")
+                {
+                    LogoutLogic();
+                }
+            }
+            else if (session.role == User)
+            {
+                TampilanMenuUtamaUser();
+                getline(cin, pilihan_pengguna);
+                if (pilihan_pengguna == "6")
+                {
+                    LogoutLogic();
+                }
+            }
+            else
+            {
+            }
+        }
+    }
 
     cout << endl;
     return 0;
 }
+#pragma endregion
